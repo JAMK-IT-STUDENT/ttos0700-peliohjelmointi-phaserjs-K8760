e12dc41f59606f0ad557bf4493adadfd2a1d1d6d
unknown <K8760@student.jamk.fi>
2018-01-29 13:35:19 +0200

    phase 0

--- viikko 4/state0.js
+++ viikko 4/state0.js
@@ -1,14 +1,58 @@
-var demo = {};
+var demo = {}, centerX = 1500/2, centerY = 800/2, adam, speed = 6;
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.spritesheet('adam', 'assets/spritesSheets/adamSheet.png', 240, 400);
+        game.load.image('tree', 'assets/backgrounds/tree.png');
+    },
     create: function(){
+		game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#DDDDDD';
         console.log('state0');
         addChangesStateEventListeners();
+		game.world.setBounds(0, 0, 2250, 800);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+		var tree = game.add.sprite(0, 0, 'tree');
+		
+		//adding character
+        adam = game.add.sprite(centerX, centerY+50, 'adam');
+        adam.anchor.setTo(0.5, 0.5);
+		adam.scale.setTo(0.6, 0.6);
+		game.physics.enable(adam);
+		adam.body.collideWorldBounds = true;
+		adam.animations.add('walk', [0,1,2,3,4]);
+		
+		//making camera to follow
+		game.camera.follow(adam);
+		game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 800);
+		
     },
-    update: function(){}
+    update: function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+			adam.scale.setTo(0.7, 0.7);
+            adam.x += speed;
+			adam.animations.play('walk', 14, true);
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+			adam.scale.setTo(-0.7, 0.7);
+            adam.x -= speed;
+			adam.animations.play('walk', 14, true);
+        }
+		else {
+			adam.animations.play('walk', 14, true);
+			adam.frame = 0;
+		}
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            adam.y -= speed;
+			if(adam.y < 420) {
+				adam.y = 420;
+			}
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            adam.y += speed;
+        }
+    }
 };
 
 function changeState(i, stateNum){
